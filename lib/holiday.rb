d11e11f1223c799cd050c899833de648676f9937
Zachary Darneille <zakdarn@gmail.com>
2020-09-19 05:07:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, holiday|
    puts "#{season.to_s.capitalize!}:"
-      binding.pry
+      #binding.pry
     end
   end
      # iterate through holiday_hash and print items such that your readout resembles:
