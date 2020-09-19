e007d11cc6878fcf90644563f964b3984e3632a7
Zachary Darneille <zakdarn@gmail.com>
2020-09-19 05:05:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, holiday|
-   puts "#{season.capitilize}:"
+   puts "#{season.capitalize}:"
       binding.pry
     end
   end
